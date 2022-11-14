aea5911c57e5820fc33787f409a6e03354fa1967
Colin G(IO) <gerriko.iot@gmail.com>
2022-11-14 16:03:31 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 /* mbed Microcontroller Library
  * Copyright (c) 2019 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
- */
+ *
+ * Application: Gesture Library test program BETA
+ * Author: C Gerrish @November 2022
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+* OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*
+* Except as contained in this notice, the name of Maxim Integrated
+* Products, Inc. shall not be used except as stated in the Maxim Integrated
+* Products, Inc. Branding Policy.
+*
+* The mere transfer of this software does not imply any licenses
+* of trade secrets, proprietary technology, copyrights, patents,
+* trademarks, maskwork rights, or any other form of intellectual
+* property whatsoever. Maxim Integrated Products, Inc. retains all
+* ownership rights.
+*/
 
 #include "mbed.h"
 #include "USBSerial.h"
@@ -11,6 +32,7 @@
 #define MAX25405_DEVICE
 
 #include "MAX25x05.h"
+#include "gesture_lib.h"
 
 #define USE_SPI 1
 
@@ -38,38 +60,51 @@
 
 #endif
 
+// Bytes prefixed to pixel data to create data frame
+const int NUM_INFO_BYTES = 40;
+
 
 int main()
 {
 
     //setup USB Serial comms for configuration option
     USBSerial serial(true, 0x0b6a, 0x4360, 0x0001);
+    serial.set_blocking (true);
 
-    MAX25x05 gesture1(MAXIObus_1, P5_3);            // Interrupt pin for sensor 1
-    //MAX25x05 gesture2(MAXIObus_2, P3_3);            // Interrupt pin for sensor 2
+    MAX25x05 max25x_1(MAXIObus_1, P5_3);            // Interrupt pin for sensor 1
+    //MAX25x05 max25x_2(MAXIObus_2, P3_3);            // Interrupt pin for sensor 2
 
-    static int pixels[NUM_SENSOR_PIXELS];
+    // Use the gesture library to manipulate/prepare pixels for output
+    // ------------------------------------------------------------------
+    gesture_lib gesture_1(SENSOR_COLS, SENSOR_ROWS);
+    //int16_t pixels[NUM_SENSOR_PIXELS] = {'\0'};
 
     // Note if using 2 gesture sensors then the LED timings need to change [TODO]
-    gesture1.set_default_register_settings();       // Define for sensor number 1
-    //gesture2.set_default_register_settings();       // Define for sensor number 2
+    max25x_1.set_default_register_settings();           // Define for sensor number 1
+    //max25x_2.set_default_register_settings();         // Define for sensor number 2
 
-    gesture1.enable_read_sensor_frames();
-    //gesture2.enable_read_sensor_frames();
+    max25x_1.enable_read_sensor_frames();
+    //max25x_2.enable_read_sensor_frames();
 
-    serial.printf("\r\nMAX25405 Comms Test\r\n");
+    serial.printf("\r\nMAX25405 Pixel Extraction and Prep Test: version 1\r\n");
 
     while (true) {
         // If using INTB interrupt, the sensorDataReadyFlag will be set when the end-of-conversion occurs
-        if (gesture1.sensorDataReadyFlag) {
-            gesture1.getSensorPixels(pixels, false);
+        if (max25x_1.sensorDataReadyFlag) {
+            // Check the interrupt pin to see if PWRON was set [[TODO]]
+            uint8_t newIntVal = 0;
+
+            max25x_1.getSensorPixelInts(gesture_1.pixels, false);
+            gesture_1.processGesture(WINDOW_FILTER_ALPHA, gesture_1.GEST_DYNAMIC);
             
             for (uint8_t i = 0; i < NUM_SENSOR_PIXELS; i++) {
-                if (i < (NUM_SENSOR_PIXELS-1)) serial.printf("%u,", pixels[i]);
-                else  serial.printf("%u\r\n", pixels[i]);
+                serial.printf("%d,", gesture_1.pixels[i]);
             }
-            gesture1.sensorDataReadyFlag = false;
+            serial.printf("%d%d\r\n",0xFFFF,0xFFFF);            // Adding placeholder for data checksum
+            
+            memset(gesture_1.pixels, '\0', NUM_SENSOR_PIXELS);
+            
+            max25x_1.sensorDataReadyFlag = false;
         }
-
     }
 }
